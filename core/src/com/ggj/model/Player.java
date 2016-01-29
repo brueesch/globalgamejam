5d6ee7c6fe0c7becb0545d4c12418630a8912490
Stephan <stephan.kessler@outlook.com>
2016-01-30 00:38:32 +0100

    created player object

---
+++ core/src/com/ggj/model/Player.java
@@ -0,0 +1,43 @@
+package com.ggj.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.utils.Array;
+
+public class Player {
+	private int spell_combo_size = 5;
+	
+	private Array<String> spell_combos;
+	
+	public Player(){
+		
+	}
+	
+	public void update(){
+		draw();
+		playerInput();
+	}
+	
+	private void draw(){
+		
+	}
+	
+	private void playerInput(){
+		if(spell_combos.size > spell_combo_size){
+			spell_combos.clear();
+		}
+		
+		if(Gdx.input.isKeyJustPressed(Keys.LEFT)){
+			spell_combos.add("left");
+		}
+		if(Gdx.input.isKeyJustPressed(Keys.UP)){
+			spell_combos.add("up");
+		}
+		if(Gdx.input.isKeyJustPressed(Keys.DOWN)){
+			spell_combos.add("down");
+		}
+		if(Gdx.input.isKeyJustPressed(Keys.RIGHT)){
+			spell_combos.add("right");
+		}
+	}
+}
