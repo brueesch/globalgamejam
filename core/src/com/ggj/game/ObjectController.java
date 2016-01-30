be1dfcef9c12e16692eaac85bf08a71795650c7c
Pascal Stählin <staehpas@students.zhaw.ch>
2016-01-30 13:43:29 +0100

    Add method to get player from ObjectController

--- core/src/com/ggj/game/ObjectController.java
+++ core/src/com/ggj/game/ObjectController.java
@@ -2,12 +2,26 @@ package com.ggj.game;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
+import com.ggj.model.Player;
 
 public class ObjectController {
   protected static synchronized int register(Actor actor) {
     objects.add(actor);
     
     return ++objectId;
+  }
+  
+  protected static Player getPlayer()
+  {
+    for(Actor actor : objects)
+    {
+      if(actor instanceof Player)
+      {
+        return (Player)actor;
+      }
+    }
+    
+    return null;
   }
   
   private static int objectId = 0;
