6d03d1105127fbd59bc613827417526fc77ee3f2
Christian <c.brueesch@yahoo.de>
2016-01-30 18:10:55 +0100

    Fixed it, and also: You'd like to meet,

--- core/src/com/ggj/game/GameSound.java
+++ core/src/com/ggj/game/GameSound.java
@@ -12,7 +12,7 @@ public class GameSound {
 
   public static Sound EARTH_ENEMY = Gdx.audio.newSound(Gdx.files.internal("sounds/earthEnemy.wav"));
   public static Sound FIRE_ENEMY = Gdx.audio.newSound(Gdx.files.internal("sounds/fireEnemy.wav"));
-  public static Sound LIGHTNING_ENEMY = Gdx.audio.newSound(Gdx.files.internal("sounds/lighntingEnemy.wav"));
+  public static Sound LIGHTNING_ENEMY = Gdx.audio.newSound(Gdx.files.internal("sounds/lightningEnemy.wav"));
   public static Sound MAGIC_SPELL1 = Gdx.audio.newSound(Gdx.files.internal("sounds/magicSpell1.wav"));
   public static Sound MAGIC_SPELL2 = Gdx.audio.newSound(Gdx.files.internal("sounds/magicSpell2.wav"));
   public static Sound WATER_ENEMIY = Gdx.audio.newSound(Gdx.files.internal("sounds/waterEnemy.wav"));
