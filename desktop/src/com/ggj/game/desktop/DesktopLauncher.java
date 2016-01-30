62c987aaa9bce3940d99647d080d16b8f4c393c6
Stephan <stephan.kessler@outlook.com>
2016-01-30 17:21:51 +0100

    added window size

--- desktop/src/com/ggj/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ggj/game/desktop/DesktopLauncher.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ggj.game.GlobalGameJam;
 
 public class DesktopLauncher {
+  
+  public static float scale = 2;
+  
   public static void main(String[] arg) {
     LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    config.width = (int)(512f * scale);
+    config.height = (int)(192f * scale);
     new LwjglApplication(new GlobalGameJam(), config);
   }
 }
