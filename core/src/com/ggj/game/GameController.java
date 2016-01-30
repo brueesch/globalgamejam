871ebe341dd20ceb3beb5cbfc86491cf630dd0c5
Stephan <stephan.kessler@outlook.com>
2016-01-30 01:14:20 +0100

    added game controller

---
+++ core/src/com/ggj/game/GameController.java
@@ -0,0 +1,73 @@
+package com.ggj.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ggj.model.Player;
+
+public class GameController implements Screen{
+	
+	private ScreenViewport screenviewport;
+	private Stage stage;
+	private OrthographicCamera camera;
+	private Player player;
+	
+	public GameController(final GlobalGameJam globalgamejam){
+		setUpGame();
+		createWorld();
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		player.update();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void createWorld(){
+		player = new Player();
+	}
+	
+	private void setUpGame(){
+		screenviewport = new ScreenViewport();
+		stage = new Stage(screenviewport);
+		Gdx.input.setInputProcessor(stage);
+		camera = (OrthographicCamera)stage.getCamera();
+	}
+}
