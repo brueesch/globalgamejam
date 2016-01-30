5dd19739cc9c548494422e9eb389f76de15f9aa3
Christian <c.brueesch@yahoo.de>
2016-01-30 11:38:51 +0100

    first config commit

---
+++ core/src/com/ggj/game/GameConfig.java
@@ -0,0 +1,49 @@
+package com.ggj.game;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by chris on 30.01.16.
+ */
+public class GameConfig {
+
+    public GameConfig() {
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("config.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("database"));
+            System.out.println(prop.getProperty("dbuser"));
+            System.out.println(prop.getProperty("dbpassword"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+}
+
